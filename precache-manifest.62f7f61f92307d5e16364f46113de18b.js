81b473848457980bb3f5982cf0c8dfe80255d300
a13x5u5 <al3xsus@pm.me>
2020-05-04 00:06:52 +0600

    Updates

---
+++ precache-manifest.62f7f61f92307d5e16364f46113de18b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a1a2338e457ebb48f7302ea22a06af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bcd7de616e60129b62d",
+    "url": "/static/css/main.79ed9682.chunk.css"
+  },
+  {
+    "revision": "23316e73c9cc9d2227a4",
+    "url": "/static/js/2.e1547b7b.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.e1547b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bcd7de616e60129b62d",
+    "url": "/static/js/main.f6faa190.chunk.js"
+  },
+  {
+    "revision": "3722e7a40be3f5dbff8e",
+    "url": "/static/js/runtime-main.b6ec3f95.js"
+  },
+  {
+    "revision": "37da0be547597a78de6b6a606b7be411",
+    "url": "/static/media/logo.37da0be5.svg"
+  }
+]);
